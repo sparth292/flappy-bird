c96872d1ac9b10a98ccb1e63f690bbb5a3d080f4
sparth292 <sparthsalunke@gmail.com>
2026-08-25 20:11:21 +0530

    Commit Using Shell.sh

--- src/main/java/com/parth/flappybird/App.java
+++ src/main/java/com/parth/flappybird/App.java
@@ -8,6 +8,10 @@ public class App {
     public static void main(String[] args) {
         JFrame gameWindow = new JFrame("Flappy Bird");
 
+        GamePanel gamePanel = new GamePanel();
+
+        gameWindow.add(gamePanel);
+        
         gameWindow.setSize(900,700);
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.setLocationRelativeTo(null);
--- src/main/java/com/parth/flappybird/GamePanel.java
+++ src/main/java/com/parth/flappybird/GamePanel.java
@@ -1,5 +1,14 @@
+//package statements
 package com.parth.flappybird;
 
-public class GamePanel {
-    
+//import statements
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+public class GamePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawString("Flappy Bird" , 100 , 100);    
+    }    
 }
